83cb28cbff37cc92537a7a2683e416ee973c866b
evanstcom <zhenyakoks@gmail.com>
2024-11-10 14:14:25 +0300

    add validate search

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -4,17 +4,21 @@
       <BackLink/>
       <h2 class="text-2xl">Поиск</h2>
     </div>
-    <div class="relative w-full mb-4">
+    <div class="relative w-full mb-4" :class="{shake : disabled}">
       <input
           ref="filter"
-          placeholder="Поиск фильмов, сериалов..."
+          :placeholder="disabled ? 'Поле не должно быть пустым' : 'Поиск фильмов, сериалов...'"
           class="w-full p-2 bg-gray-900 rounded-2xl py-2 pr-10 pl-4 outline-none flex items-center"
           type="text" v-model="searchValue" @keydown.enter="goToSearch">
+      <button v-show="searchValue.length > 0" class="absolute right-20 top-1/2 -translate-y-1/2 mr-1"
+              @click="searchValue = ''"><img class="w-4" src="/clear.svg" alt="clear">
+      </button>
       <button class="absolute right-0 top-1/2 -translate-y-1/2 bg-orange-600 py-2 px-3 rounded-2xl"
               @click="goToSearch">Найти
       </button>
     </div>
-    <SliderList :films="searchFilms"/>
+    <SliderList v-if="searchFilms.length > 0" :films="searchFilms"/>
+    <!--    <div v-else class="text-lg text-center">По вашему запросу ничего не нашлось</div>-->
   </section>
 </template>
 
@@ -36,10 +40,21 @@ const getItemSearch = async (q) => {
         },
       })
   searchFilms.value = data.films
+  console.log(data.films)
 }
 const goToSearch = () => {
-  if (!searchValue.value || searchValue.value < 2) return
+  if (!searchValue.value || searchValue.value < 2) return warnDisabled()
   getItemSearch(searchValue.value)
+}
+
+//ошибки если инпут пустой
+const disabled = ref(false)
+
+function warnDisabled() {
+  disabled.value = true
+  setTimeout(() => {
+    disabled.value = false
+  }, 1500)
 }
 
 onMounted(() => {
@@ -48,3 +63,33 @@ onMounted(() => {
   })
 })
 </script>
+
+<style scoped lang="scss">
+.shake {
+  animation: shake 0.82s cubic-bezier(0.36, 0.07, 0.19, 0.97) both;
+  transform: translate3d(0, 0, 0);
+}
+
+@keyframes shake {
+  10%,
+  90% {
+    transform: translate3d(-1px, 0, 0);
+  }
+
+  20%,
+  80% {
+    transform: translate3d(2px, 0, 0);
+  }
+
+  30%,
+  50%,
+  70% {
+    transform: translate3d(-4px, 0, 0);
+  }
+
+  40%,
+  60% {
+    transform: translate3d(4px, 0, 0);
+  }
+}
+</style>
--- src/components/SliderItem.vue
+++ src/components/SliderItem.vue
@@ -1,9 +1,11 @@
 <template>
   <RouterLink :to="{ name: 'item', params: {id: film.kinopoiskId ? film.kinopoiskId : film.filmId}}"
               class="flex flex-col">
-    <div class="mb-1 image bg-cover rounded-md" :style="`background-image: url(${film.posterUrlPreview});`"
+    <div class="mb-1 image bg-contain rounded-md"
          :class="size">
-      <!--      <img class="rounded-md mb-1 h-full" :src="film.posterUrl" :alt="film.nameRu">-->
+      <!--      <div class="mb-1 image bg-contain rounded-md" :style="`background-image: url(${film.posterUrlPreview});`"
+               :class="size">-->
+      <img class="rounded-md mb-1 h-full w-full" :src="film.posterUrl" :alt="film.nameRu">
     </div>
     <p class="text-xs text-slate-400 mb-1">{{ film.year }}, {{ film.genres[0].genre }}</p>
     <h3 class="text-xs mb-1">{{ film.nameRu }}</h3>
@@ -16,7 +18,7 @@ defineProps({
       film: Object,
       size: {
         type: String,
-        default: 'w-72 h-96',
+        default: 'w-64 h-96',
         required: false
       }
     }
