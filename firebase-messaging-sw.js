a299fa2f12bcab05a96ba225541f37f0cfbd0b5d
evanstcom <zhenyakoks@gmail.com>
2025-01-11 00:26:50 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,15 @@
-if(!self.define){let e,s={};const i=(i,n)=>(i=new URL(i+".js",n).href,s[i]||new Promise((s=>{if("document"in self){const e=document.createElement("script");e.src=i,e.onload=s,document.head.appendChild(e)}else e=i,importScripts(i),s()})).then((()=>{let e=s[i];if(!e)throw new Error(`Module ${i} didn’t register its module`);return e})));self.define=(n,r)=>{const t=e||("document"in self?document.currentScript.src:"")||location.href;if(s[t])return;let o={};const a=e=>i(e,t),c={module:{uri:t},exports:o,require:a};s[t]=Promise.all(n.map((e=>c[e]||a(e)))).then((e=>(r(...e),o)))}}define(["./workbox-74f2ef77"],(function(e){"use strict";self.skipWaiting(),e.clientsClaim(),e.precacheAndRoute([{url:"assets/index-CdOUMzvp.css",revision:null},{url:"assets/index-CUr6eOBL.js",revision:null},{url:"firebase-messaging-sw.js",revision:"00a68141e992dfcabdca8e8105f7b99c"},{url:"index.html",revision:"81ca5d8213be82a957ab680ebd865696"},{url:"registerSW.js",revision:"7bf5bea73aab35aa99eb0b9a000b981a"},{url:"img/icons/192x192.png",revision:"03e4cf194e61388a438209e792d4d896"},{url:"img/icons/512x512.png",revision:"0a12c449b27b28c081403639786aa7b5"},{url:"manifest.webmanifest",revision:"e3c375b09d8a7cf62944854f1a0522ad"}],{}),e.cleanupOutdatedCaches(),e.registerRoute(new e.NavigationRoute(e.createHandlerBoundToURL("index.html"))),e.registerRoute(/.*\.(png|jpg|jpeg|svg|gif|css|js)/,new e.CacheFirst({cacheName:"assets",plugins:[new e.ExpirationPlugin({maxEntries:50,maxAgeSeconds:600})]}),"GET")}));
+import {initializeApp} from 'firebase/app';
+import {getMessaging} from 'firebase/messaging';
+
+const firebaseMessagingSw = {
+    apiKey: "AIzaSyD036jwFy2asDyY4AvY6UIQwXwzejynCfM",
+    authDomain: "filmoteka-by-evans.firebaseapp.com",
+    projectId: "filmoteka-by-evans",
+    storageBucket: "filmoteka-by-evans.firebasestorage.app",
+    messagingSenderId: "652707479711",
+    appId: "1:652707479711:web:b7185b679a324a2b2a62aa"
+};
+const app = initializeApp(firebaseMessagingSw);
+const messaging = getMessaging(app);
+
+export {messaging};
